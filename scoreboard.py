9be4019050b02dc06dc12a0186a3768ccad13784
Rahul Kumar <75177680+RahulK199@users.noreply.github.com>
2020-11-30 19:13:05 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -18,7 +18,7 @@ class Scoreboard():
 		  self.prep_level()
 		  self.prep_ships()
 	def prep_score(self):	  
-		  """Turn the score into a rendered image."""
+		  """Turns the score into a rendered image."""
 		  rounded_score = int(round(self.stats.score, -1))
 		  score_str = "{:,}".format(rounded_score)
 		  self.score_image = self.font.render(score_str, True, self.text_color, self.ai_settings.bg_color)			
@@ -27,14 +27,14 @@ class Scoreboard():
 		  self.score_rect.right = self.screen_rect.right - 20
 		  self.score_rect.top = 20		  
 	def show_score(self):	  
-		  """Draw scores and ships to the screen."""
+		  """Draws scores and ships to the screen."""
 		  self.screen.blit(self.score_image, self.score_rect)	
 		  self.screen.blit(self.high_score_image, self.high_score_rect)
 		  self.screen.blit(self.level_image, self.level_rect)
 		  # Draw ships.
 		  self.ships.draw(self.screen)
 	def prep_high_score(self):	  
-	      """Turn the high score into a rendered image."""
+	      """Turns the high score into a rendered image."""
 	      high_score = int(round(self.stats.high_score, -1))
 	      high_score_str = "{:,}".format(high_score)
 	      self.high_score_image = self.font.render(high_score_str, True,self.text_color, self.ai_settings.bg_color)
@@ -43,7 +43,7 @@ class Scoreboard():
 	      self.high_score_rect.centerx = self.screen_rect.centerx
 	      self.high_score_rect.top = self.score_rect.top
 	def prep_ships(self):
-		  """Show how many ships are left."""
+		  """Shows how many ships are left."""
 		  self.ships = Group()
 		  for ship_number in range(self.stats.ships_left):
 			    ship = Ship(self.ai_settings, self.screen)

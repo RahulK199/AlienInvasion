447d30442bea1e4de6cb44a4e0a6774170b68094
Rahul Kumar <75177680+RahulK199@users.noreply.github.com>
2020-11-30 19:07:45 +0530

    Update button.py

--- button.py
+++ button.py
@@ -2,17 +2,17 @@ import pygame.font
 
 class Button():
      def __init__(self, ai_settings, screen, msg):
-         """Initialize button attributes."""
+         """Initializes button attributes."""
          self.screen = screen
          self.screen_rect = screen.get_rect()
          
-         # Set the dimensions and properties of the button.
+         # Sets the dimensions and properties of the button.
          self.width, self.height = 200, 50
          self.button_color = (0, 255, 0)
          self.text_color = (255, 255, 255)
          self.font = pygame.font.SysFont(None, 48)
          
-         # Build the button's rect object and center it.	
+         # Builds the button's rect object and center it.	
          self.rect = pygame.Rect(0, 0, self.width, self.height)
          self.rect.center = self.screen_rect.center
          
@@ -26,6 +26,6 @@ class Button():
          self.msg_image_rect.center = self.rect.center
                     
      def draw_button(self):
-         # Draw blank button and then draw message.
+         # Draws blank button and then draws message.
          self.screen.fill(self.button_color, self.rect)
          self.screen.blit(self.msg_image, self.msg_image_rect)       
